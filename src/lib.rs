3d72f3b7233f8ee1ae69c041207c4868fdf57c03
Mozzie <mozzie@mozzie.local>
2024-04-25 23:46:06 -0500

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -12,10 +12,7 @@ struct Stack<T: Clone> {
 
 impl<T: Clone> Stack<T> {
     fn new() -> Self {
-        Stack {
-            head: None,
-            size: 0,
-        }
+        Stack { head: None, size: 0, }
     }
 
     fn push(&mut self, item: T) {
@@ -35,9 +32,7 @@ impl<T: Clone> Stack<T> {
         }
     }
 
-    fn len(&self) -> isize {
-        self.size
-    }
+    fn len(&self) -> isize { self.size }
 }
 
 // individual nodes in the stack, constructed in the style of a linked list
@@ -52,26 +47,22 @@ impl<T: Clone> Node<T> {
         Node { item, next }
     }
 
-    fn get_item(&self) -> T {
-        self.item.clone()
-    }
-    fn get_next(&self) -> NodePtr<T> {
-        self.next.clone()
-    }
+    fn get_item(&self) -> T          { self.item.clone() }
+    fn get_next(&self) -> NodePtr<T> { self.next.clone() }
 }
 
 // evaluates the expression formed by the operator and 2 elements
 fn evaluate(a: f64, b: f64, operator: String) -> f64 {
     match operator.as_str() {
-        "+" => a + b,
-        "-" => a - b,
-        "*" => a * b,
-        "/" => a / b,
-        "%" => a % b,
-        "^" => a.powf(b),
-        "E" => a * f64::powf(10.0, b),
+        "+"   => a + b,
+        "-"   => a - b,
+        "*"   => a * b,
+        "/"   => a / b,
+        "%"   => a % b,
+        "^"   => a.powf(b),
+        "E"   => a * f64::powf(10.0, b),
         "log" => a.log(b),
-        _ => panic!("{} is not an operator", operator),
+        _     => panic!("{} is not an operator", operator),
     }
 }
 
@@ -79,25 +70,18 @@ fn evaluate(a: f64, b: f64, operator: String) -> f64 {
 // notation
 pub fn solve(expression: Vec<String>) -> f64 {
     let mut cache: Stack<f64> = Stack::new();
-
     for e in expression {
         match e.parse::<f64>() {
-            Ok(ok) => cache.push(ok),
-            Err(_) if e == "e" => cache.push(std::f64::consts::E),
+            Ok(ok)              => cache.push(ok),
+            Err(_) if e == "e"  => cache.push(std::f64::consts::E),
             Err(_) if e == "pi" => cache.push(std::f64::consts::PI),
-            Err(_) => {
-                if cache.len() < 2 {
-                    panic!("too few numbers supplied to operator: {}", e);
-                }
+            Err(_)              => {
+                if cache.len() < 2 { panic!("too few arguments for {}", e); }
                 let (b, a) = (cache.pop().unwrap(), cache.pop().unwrap());
                 cache.push(evaluate(a, b, e));
             }
         }
     }
-
-    if cache.len() == 1 {
-        cache.pop().unwrap()
-    } else {
-        panic!("values left unused after operations completed");
-    }
+    if cache.len() != 1 { panic!("values left unused after evaluation"); }
+    cache.pop().unwrap()
 }
