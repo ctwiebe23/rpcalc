c9c8313379cbbb777c4d7967c9dedf3b37c52372
Mozzie <mozzie@mozzie.local>
2024-04-25 23:14:50 -0500

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use rpcalc::solve;
 
 fn main() {
-    let expression: Vec<String> = std::env::args().skip(1).collect();
+    let expression: Vec<String> = std::env::args()
+        .collect::<Vec<String>>()[1]
+        .split(" ")
+        .map(|s| s.to_string())
+        .collect();
+
     print!("{}\n", solve(expression));
 }
