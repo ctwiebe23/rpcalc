686c00ddd58bc05b9ac675a21dc2b6bd57854d38
Mozzie <mozzie@mozzie.local>
2024-04-25 22:22:02 -0500

    initial build finished

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,30 @@
 type NodePtr<T> = Option<Box<Node<T>>>;
 
-pub struct Stack<T: Clone> {
+// simple implementation of the standard stack data structure
+//
+// features push, pop, and len
+//
+// cannot be constructed from an existing collection
+struct Stack<T: Clone> {
     head: NodePtr<T>,
     size: isize,
 }
 
 impl<T: Clone> Stack<T> {
-    pub fn new() -> Self {
-        Stack { head: None, size: 0, }
+    fn new() -> Self {
+        Stack {
+            head: None,
+            size: 0,
+        }
     }
 
-    pub fn push(&mut self, item: T) {
+    fn push(&mut self, item: T) {
         let prev: NodePtr<T> = self.head.clone();
         self.head = Some(Box::new(Node::new(item, prev)));
         self.size += 1;
     }
 
-    pub fn pop(&mut self) -> Option<T> {
+    fn pop(&mut self) -> Option<T> {
         match self.head.take() {
             None => None,
             Some(popped) => {
@@ -27,9 +35,12 @@ impl<T: Clone> Stack<T> {
         }
     }
 
-    pub fn len(&self) -> isize { self.size }
+    fn len(&self) -> isize {
+        self.size
+    }
 }
 
+// individual nodes in the stack, constructed in the style of a linked list
 #[derive(Clone)]
 struct Node<T: Clone> {
     item: T,
@@ -41,6 +52,52 @@ impl<T: Clone> Node<T> {
         Node { item, next }
     }
 
-    fn get_item(&self) -> T          { self.item.clone() }
-    fn get_next(&self) -> NodePtr<T> { self.next.clone() }
+    fn get_item(&self) -> T {
+        self.item.clone()
+    }
+    fn get_next(&self) -> NodePtr<T> {
+        self.next.clone()
+    }
+}
+
+// evaluates the expression formed by the operator and 2 elements
+fn evaluate(a: f64, b: f64, operator: String) -> f64 {
+    match operator.as_str() {
+        "+" => a + b,
+        "-" => a - b,
+        "*" => a * b,
+        "/" => a / b,
+        "%" => a % b,
+        "^" => a.powf(b),
+        "E" => a * f64::powf(10.0, b),
+        "log" => a.log(b),
+        _ => panic!("{} is not an operator", operator),
+    }
+}
+
+// solves the expression represented by a vector of strings in reverse polish
+// notation
+pub fn solve(expression: Vec<String>) -> f64 {
+    let mut cache: Stack<f64> = Stack::new();
+
+    for e in expression {
+        match e.parse::<f64>() {
+            Ok(ok) => cache.push(ok),
+            Err(_) if e == "e" => cache.push(std::f64::consts::E),
+            Err(_) if e == "pi" => cache.push(std::f64::consts::PI),
+            Err(_) => {
+                if cache.len() < 2 {
+                    panic!("too few numbers supplied to operator: {}", e);
+                }
+                let (b, a) = (cache.pop().unwrap(), cache.pop().unwrap());
+                cache.push(evaluate(a, b, e));
+            }
+        }
+    }
+
+    if cache.len() == 1 {
+        cache.pop().unwrap()
+    } else {
+        panic!("values left unused after operations completed");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-use rpcalc::Stack;
+use rpcalc::solve;
 
 fn main() {
-    let mut stack: Stack<i32> = Stack::new();
-    stack.push(32);
-    stack.push(42);
-    stack.push(10);
-    stack.push(3);
-    println!("{:?}, size = {:?}", stack.pop().unwrap_or(0), stack.len());
-    println!("{:?}, size = {:?}", stack.pop().unwrap_or(0), stack.len());
-    println!("{:?}, size = {:?}", stack.pop().unwrap_or(0), stack.len());
-    println!("{:?}, size = {:?}", stack.pop().unwrap_or(0), stack.len());
-    println!("{:?}, size = {:?}", stack.pop().unwrap_or(0), stack.len());
-    println!("{:?}, size = {:?}", stack.pop().unwrap_or(0), stack.len());
-    println!("{:?}, size = {:?}", stack.pop().unwrap_or(0), stack.len());
-    println!("{:?}, size = {:?}", stack.pop().unwrap_or(0), stack.len());
+    let expression: Vec<String> = std::env::args().skip(1).collect();
+    print!("{}\n", solve(expression));
 }
