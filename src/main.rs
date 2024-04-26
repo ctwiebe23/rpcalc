c44213085e68b2c60aefa98c2689e1deea8b61d4
Mozzie <mozzie@mozzie.local>
2024-04-26 12:57:59 -0500

    formatting

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,10 @@ use rpcalc::solve;
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
     let mut expression: Vec<String> = Vec::new();
-
     for a in args {
         expression.append(&mut a.split(" ")
             .map(|s| s.to_string())
             .collect());
     }
-
     print!("{}\n", solve(expression));
 }
