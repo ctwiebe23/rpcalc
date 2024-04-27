2d75f504a0eab2bbf2bf575a0ad9cfc2ac1b6fd1
Mozzie <mozzie@mozzie.local>
2024-04-26 20:20:46 -0500

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -52,52 +52,60 @@ impl<T: Clone> Node<T> {
 }
 
 // evaluates the expression formed by the operator and 2 elements
-fn evaluate(cache: &mut Stack<f64>, operator: String) -> f64 {
-    let safe_pop = |c: &mut Stack<f64>| match c.pop() {
-        None => panic!("too few arguments supplied for {}", operator),
-        Some(x) => x,
+fn evaluate(cache: &mut Stack<f64>, operator: String) -> Result<f64, String> {
+
+    let safe_pop = |stack: &mut Stack<f64>| match stack.pop() {
+        None => Err(format!("too few arguments for {operator}")),
+        Some(x) => Ok(x),
     };
-    let rad = |r: f64| r % (2.0 * std::f64::consts::PI);
-    let b = safe_pop(cache);
+
+    let b = safe_pop(cache)?;
     match operator.as_str() {
-        "+" => safe_pop(cache) + b,
-        "-" => safe_pop(cache) - b,
-        "*" | "x" => safe_pop(cache) * b,
-        "/" => safe_pop(cache) / b,
-        "%" => safe_pop(cache) % b,
-        "^" => safe_pop(cache).powf(b),
-        "log" => safe_pop(cache).log(b),
-        "sin" => rad(b).sin(),
-        "cos" => rad(b).cos(),
-        "tan" => rad(b).tan(),
-        "csc" => 1.0 / rad(b).sin(),
-        "sec" => 1.0 / rad(b).cos(),
-        "cot" => 1.0 / rad(b).tan(),
-        "arcsin" => rad(b).asin(),
-        "arccos" => rad(b).acos(),
-        "arctan" => rad(b).atan(),
-        "arccsc" => 1.0 / rad(b).asin(),
-        "arcsec" => 1.0 / rad(b).acos(),
-        "arccot" => 1.0 / rad(b).atan(),
-        _ => panic!("{} is not an operator", operator),
+        "+" => Ok(safe_pop(cache)? + b),
+        "-" => Ok(safe_pop(cache)? - b),
+        "*" | "x" => Ok(safe_pop(cache)? * b),
+        "/" => Ok(safe_pop(cache)? / b),
+        "%" => Ok(safe_pop(cache)? % b),
+        "^" => Ok(safe_pop(cache)?.powf(b)),
+        "log" => Ok(safe_pop(cache)?.log(b)),
+        "ln" => Ok(b.ln()),
+        "sin" => Ok(b.sin()),
+        "cos" => Ok(b.cos()),
+        "tan" => Ok(b.tan()),
+        "csc" => Ok(1.0 / b.sin()),
+        "sec" => Ok(1.0 / b.cos()),
+        "cot" => Ok(1.0 / b.tan()),
+        "arcsin" => Ok(b.asin()),
+        "arccos" => Ok(b.acos()),
+        "arctan" => Ok(b.atan()),
+        "arccsc" => Ok(1.0 / b.asin()),
+        "arcsec" => Ok(1.0 / b.acos()),
+        "arccot" => Ok(1.0 / b.atan()),
+        _ => Err(format!("{operator} is not an operator")),
     }
 }
 
 // solves the expression represented by a vector of strings in reverse polish
 // notation
-pub fn solve(expression: Vec<String>) -> f64 {
+pub fn solve(expression: Vec<String>) -> Result<f64, String> {
+
     let mut cache: Stack<f64> = Stack::new();
+
     for v in expression {
         match v.parse::<f64>() {
             Ok(ok) => cache.push(ok),
             Err(_) if v == "e" => cache.push(std::f64::consts::E),
             Err(_) if v == "pi" => cache.push(std::f64::consts::PI),
             Err(_) => {
-                let result = evaluate(&mut cache, v);
-                cache.push(result);
+                let result = evaluate(&mut cache, v)?;
+                cache.push(result)
             }
         }
     }
-    if cache.len() != 1 { panic!("values left unused after evaluation"); }
-    cache.pop().unwrap()
+
+    match cache.len() {
+        1 => Ok(cache.pop().unwrap()),
+        0 => Err("cache empty; no values given".to_string()),
+        _ => Err("invalid syntax; values left unused".to_string()),
+    }
 }
