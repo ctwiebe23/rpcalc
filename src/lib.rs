e0811989a393776bac0dadadbd277c42bc0c466f
Mozzie <mozzie@mozzie.local>
2024-05-21 17:45:14 -0500

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// mozzie
+//
+// contains both a stack and a postfix calculator
+
 type NodePtr<T> = Option<Box<Node<T>>>;
 
 // simple implementation of the standard stack data structure
@@ -6,27 +10,33 @@ type NodePtr<T> = Option<Box<Node<T>>>;
 //
 // cannot be constructed from an existing collection
 struct Stack<T: Clone> {
+
     head: NodePtr<T>,
     size: isize,
 }
 
 impl<T: Clone> Stack<T> {
+
     fn new() -> Self {
         Stack { head: None, size: 0, }
     }
 
     fn push(&mut self, item: T) {
+
         let prev: NodePtr<T> = self.head.clone();
+
         self.head = Some(Box::new(Node::new(item, prev)));
         self.size += 1;
     }
 
     fn pop(&mut self) -> Option<T> {
+
         match self.head.take() {
-            None => None,
+            None         => None,
             Some(popped) => {
                 self.head = popped.get_next();
                 self.size -= 1;
+
                 Some(popped.get_item())
             }
         }
@@ -38,16 +48,18 @@ impl<T: Clone> Stack<T> {
 // individual nodes in the stack, constructed in the style of a linked list
 #[derive(Clone)]
 struct Node<T: Clone> {
+
     item: T,
     next: NodePtr<T>,
 }
 
 impl<T: Clone> Node<T> {
+
     fn new(item: T, next: NodePtr<T>) -> Self {
         Node { item, next }
     }
 
-    fn get_item(&self) -> T { self.item.clone() }
+    fn get_item(&self) -> T          { self.item.clone() }
     fn get_next(&self) -> NodePtr<T> { self.next.clone() }
 }
 
@@ -67,7 +79,7 @@ fn factorial(n: f64) -> Result<f64, String> {
     }
 
     let mut result: f64 = 1.0;
-    let limit: u32 = (n as u32) + 1;
+    let     limit:  u32 = (n as u32) + 1;
 
     for i in 1..limit {
         result *= i as f64;
@@ -80,37 +92,38 @@ fn factorial(n: f64) -> Result<f64, String> {
 fn evaluate(cache: &mut Stack<f64>, operator: String) -> Result<f64, String> {
 
     let safe_pop = |stack: &mut Stack<f64>| match stack.pop() {
-        None => Err(format!("too few arguments for {operator}")),
+        None    => Err(format!("too few arguments for {operator}")),
         Some(x) => Ok(x),
     };
 
-    let b = safe_pop(cache)?;
+    let b: f64 = safe_pop(cache)?;
+
     match operator.as_str() {
-        "+" => Ok(safe_pop(cache)? + b),
-        "-" => Ok(safe_pop(cache)? - b),
+        "+"       => Ok(safe_pop(cache)? + b),
+        "-"       => Ok(safe_pop(cache)? - b),
         "*" | "x" => Ok(safe_pop(cache)? * b),
-        "/" => Ok(safe_pop(cache)? / b),
-        "%" => Ok(safe_pop(cache)? % b),
-        "^" => Ok(safe_pop(cache)?.powf(b)),
-        "log" => Ok(safe_pop(cache)?.log(b)),
-        "rnd" => Ok(rnd(safe_pop(cache)?, b)),
-        "ln" => Ok(b.ln()),
-        "log2" => Ok(b.log2()),
-        "log10" => Ok(b.log10()),
-        "sin" => Ok(b.sin()),
-        "cos" => Ok(b.cos()),
-        "tan" => Ok(b.tan()),
-        "csc" => Ok(1.0 / b.sin()),
-        "sec" => Ok(1.0 / b.cos()),
-        "cot" => Ok(1.0 / b.tan()),
-        "arcsin" => Ok(b.asin()),
-        "arccos" => Ok(b.acos()),
-        "arctan" => Ok(b.atan()),
-        "arccsc" => Ok(1.0 / b.asin()),
-        "arcsec" => Ok(1.0 / b.acos()),
-        "arccot" => Ok(1.0 / b.atan()),
-        "!" => factorial(b.round()),
-        _ => Err(format!("{operator} is not an operator")),
+        "/"       => Ok(safe_pop(cache)? / b),
+        "%"       => Ok(safe_pop(cache)? % b),
+        "^"       => Ok(safe_pop(cache)?.powf(b)),
+        "log"     => Ok(safe_pop(cache)?.log(b)),
+        "rnd"     => Ok(rnd(safe_pop(cache)?, b)),
+        "ln"      => Ok(b.ln()),
+        "log2"    => Ok(b.log2()),
+        "log10"   => Ok(b.log10()),
+        "sin"     => Ok(b.sin()),
+        "cos"     => Ok(b.cos()),
+        "tan"     => Ok(b.tan()),
+        "csc"     => Ok(1.0 / b.sin()),
+        "sec"     => Ok(1.0 / b.cos()),
+        "cot"     => Ok(1.0 / b.tan()),
+        "arcsin"  => Ok(b.asin()),
+        "arccos"  => Ok(b.acos()),
+        "arctan"  => Ok(b.atan()),
+        "arccsc"  => Ok(1.0 / b.asin()),
+        "arcsec"  => Ok(1.0 / b.acos()),
+        "arccot"  => Ok(1.0 / b.atan()),
+        "!"       => factorial(b.round()),
+        _         => Err(format!("{operator} is not an operator")),
     }
 }
 
@@ -122,14 +135,14 @@ pub fn solve(expression: Vec<String>) -> Result<f64, String> {
 
     for v in expression {
         match v.parse::<f64>() {
-            Ok(ok) => cache.push(ok),
-            Err(_) if v == "e" => cache.push(std::f64::consts::E),
-            Err(_) if v == "G" => cache.push(6.6743E-11),
-            Err(_) if v == "g" => cache.push(9.81),
-            Err(_) if v == "c" => cache.push(299792458.0),
+            Ok(ok)              => cache.push(ok),
+            Err(_) if v == "e"  => cache.push(std::f64::consts::E),
+            Err(_) if v == "G"  => cache.push(6.6743E-11),
+            Err(_) if v == "g"  => cache.push(9.81),
+            Err(_) if v == "c"  => cache.push(299792458.0),
             Err(_) if v == "pi" => cache.push(std::f64::consts::PI),
-            Err(_) => {
-                let result = evaluate(&mut cache, v)?;
+            Err(_)              => {
+                let result: f64 = evaluate(&mut cache, v)?;
                 cache.push(result)
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use rpcalc::solve;
 
 fn main() {
 
-    let args: Vec<String> = std::env::args().skip(1).collect();
-
+    let     args:       Vec<String> = std::env::args().skip(1).collect();
     let mut expression: Vec<String> = Vec::new();
+
     for a in args {
         expression.append(&mut a.split(" ")
             .map(|s| s.to_string())
